b05af59bbf90a99aeb606e5d475540645a15764e
megh-23steelsoul <100343594+megh-23steelsoul@users.noreply.github.com>
2022-03-03 15:44:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+var trex ,trex_running;
+var ground;
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png")
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  trex= createSprite(50,180,20,50);
+  //create a trex sprite
+ trex.addAnimation("running",trex_running);
+ trex.scale=0.5;
+ //create a ground sprite
+ ground = createSprite(200,180,400,20);
+}
+
+function draw(){
+  background("white")
+
+
+
+if (keyDown("space")){
+  trex.velocityY=-10;
+}
+trex.velocityY=trex.velocityY+0.3;
+// making trex not falling down 
+trex.collide(ground);
+
+
+
+  drawSprites();
+
+
+
+
+
+
+}
